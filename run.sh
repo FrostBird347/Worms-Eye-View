587b61da7b5a4f9b691a4d0a656e8973d63de4f0
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2024-05-09 23:41:24 +0800

    mod can now be built

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# I have to use this script to test the mod because I can't get BTA to launch outside of prism
+cd "$(dirname "$0")" &&
+ModName="$(cat gradle.properties | grep "mod_name" | cut -d "=" -f2)" &&
+ModVersion="$(cat gradle.properties | grep "mod_version" | cut -d "=" -f2)" &&
+./gradlew build &&
+cp "build/libs/${ModName}-${ModVersion}.jar" "/Users/frostbird/Library/Application Support/PrismLauncher/instances/bta_babric_instance_7.1_mod_debug/.minecraft/mods/${ModName}-${ModVersion}.jar" &&
+/Applications/Prism\ Launcher.app/Contents/MacOS/prismlauncher -l "bta_babric_instance_7.1_mod_debug"
