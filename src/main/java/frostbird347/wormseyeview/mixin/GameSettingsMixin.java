3489293c535c6c8e5da2e1fdf30c50697d2667e3
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2024-05-10 20:40:22 +0800

    Messy temporary shaders
    Using the override shaders isn't great, as I have to extract files from the game, but it works

--- src/main/java/frostbird347/wormseyeview/mixin/GameSettingsMixin.java
+++ src/main/java/frostbird347/wormseyeview/mixin/GameSettingsMixin.java
@@ -13,6 +13,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import frostbird347.wormseyeview.MainMod;
 import frostbird347.wormseyeview.ModOptions;
 
 @Mixin(value = {GameSettings.class}, remap = false)
@@ -22,6 +23,7 @@ public class GameSettingsMixin implements ModOptions {
 	public Minecraft mc;
 	
 	private final GameSettings mixinInst = (GameSettings)((Object)this);
+	private int lastShader = -1;
 
 	@Unique
 	public IntegerOption shader = new IntegerOption(this.mixinInst, "frostbird347.wormseyeview.shader", 0);
@@ -31,17 +33,36 @@ public class GameSettingsMixin implements ModOptions {
 	
 	@Inject(method = {"getDisplayString(Lnet/minecraft/client/option/Option;)Ljava/lang/String;"}, at = {@At("HEAD")}, cancellable = true)
 	private void displayString(Option<?> option, CallbackInfoReturnable<String> cir) {
+		
 		if (option == this.shaderIntensity) {
 			cir.setReturnValue((int)(((Float)this.shaderIntensity.value).floatValue() * 100f) + "%"); 
 		} else if (option == this.shader) {
+			if (mixinInst.shaderOverride.value != null) {
+				MainMod.LOGGER.info(mixinInst.shaderOverride.value);
+			} else {
+				MainMod.LOGGER.info("null");
+			}
+
 			if (this.shader.value >= PhotoModeRenderer.shaders.length || PhotoModeRenderer.shaders[this.shader.value] == null) {
 				this.shader.set(0);
+				mixinInst.shaderOverride.set(null);
 				cir.setReturnValue("none");
 			} else {
+				mixinInst.shaderOverride.set("photo/" + PhotoModeRenderer.shaders[this.shader.value] + "/");
 				cir.setReturnValue(PhotoModeRenderer.shaders[this.shader.value]);
+			}
+
+			if (this.shader.value != lastShader) {
+				lastShader = this.shader.value;
+				mc.render.reload();
 			}
 		}
 	}
+
+	//@Inject(method = {"optionChanged(Lnet/minecraft/client/option/Option;)V;"}, at = {@At("TAIL")})
+	//public void changeSetting(Option<?> option) {
+	//	
+	//}
 
 	@Override
 	public IntegerOption shader() {
